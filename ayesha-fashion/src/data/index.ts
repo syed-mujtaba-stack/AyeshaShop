782ffb1cbb77e062c536a0322f4509b8bc056621
Syed Mujtaba Abbas Zaidi <166711387+syed-mujtaba-stack@users.noreply.github.com>
2026-07-11 23:15:05 +0500

    Delete ayesha-fashion directory

--- ayesha-fashion/src/data/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./products";
-export * from "./reviews";
-export * from "./orders";
-export * from "./blog";
-export * from "./faq";
-export * from "./testimonials";
-export * from "./customers";
